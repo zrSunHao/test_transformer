73c2b1c2c20735e15f2ce4aa9eb0341bfab3fda7
sunhao <2080680175@qq.com>
2023-04-26 16:25:36 +0800

    positional encoding.

---
+++ config/default_cfg.py
@@ -0,0 +1,22 @@
+class DefaultConfig(object):
+
+    device = 'cuda'                         # 设备
+    print_every = 200                       # 进度输出频率
+    epoch_max = 40                          # 训练轮次
+    epoch_current = 1                       # 当前轮次
+
+    data_root = 'D:/WorkSpace/DataSet'      # 训练集根目录
+    net_save_root = './checkpoints'         # 模型参数保存目录
+    net_path = 'transformer_.pth'           # 训练好的模型
+
+    image_size = 256                        # 图像尺寸
+    batch_size = 4                          # 每批次图像数量
+    num_workers = 0                         # 进程数
+    lr = 5e-5                               # 学习率
+
+    d_model = 512                           # 字 Embedding 的维度
+    d_ff = 2048                             # 前向传播隐藏层维度
+    d_k = d_v = 64                          # K(=Q), V的维度 
+    n_layers = 6                            # 有多少个encoder和decoder
+    n_heads = 8                             # Multi-Head Attention设置为8                        
+    
---
+++ tools/positional_encoding.py
@@ -0,0 +1,59 @@
+import torch.nn as nn
+import torch as t
+import math
+
+# 词向量添加位置编码
+class PositionalEncoding(nn.Module):
+
+    '''
+    d_model:    模型的输入维度（词向量的维度）
+    droupout:   droupout 的概率
+                作为训练神经网络的一种trick选择,
+                在每个训练批次中,以某种概率忽略一定数量的神经元.
+                可以明显地减少过拟合现象.
+    max_len:    句子的最大长度
+    '''
+    def __init__(self, d_model, droupout=0.1, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+
+        self.dropout = nn.Dropout(p=droupout)
+
+        # 计算位置编码值
+        # 生成 [max_len, d_model] 大小的矩阵
+        pe = t.zeros(max_len, d_model)           # [5000,512]  
+        # 生成pos， [ [0],[1], ··· [max_len-1] ] 的张量
+        pos = t.arange(0, max_len).unsqueeze(1)  # [5000,1]，词向量在句子中的位置信息
+        m2i = t.arange(0, d_model, 2)       # 生成 2i，i为词向量内维度的位置,i在0-255之间
+        log = math.log(10000.0)/d_model     # 生成 log(10000)/d_model
+        # [256]  生成位置公式的公共部分，即 10000**(2i/d_model)
+        div_term = t.exp(m2i * -log)        
+
+        '''
+        pe 对于所有词向量，从词向量的第0个维度开始，间隔为2开始赋值，即偶数位
+        pos*div_term:[5000,256]  
+        pe:[5000,512]
+        '''
+        pe[:, 0::2] = t.sin(pos * div_term)
+        '''
+        pe 对于所有词向量，从词向量的第1个维度开始，间隔为2开始赋值，即奇数位
+        pos*div_term:[5000,256]  
+        pe:[5000,512]
+        '''
+        pe[:, 1::2] = t.cos(pos * div_term)
+        print((pos * div_term)[1, :].size())
+        pe = pe.unsqueeze(0)        # [1,5000,512]         
+        self.pe = pe
+
+    def forward(self, x):
+        num = x.size(0)             # 句子中词向量的个数
+        x = x + self.pe[:, :num]    # 词向量与位置编码相加
+        out = self.dropout(x)
+        return out
+
+
+po = PositionalEncoding(512)
+a = t.ones(0,512)
+a.unsqueeze(1)
+po(a)
+
+
