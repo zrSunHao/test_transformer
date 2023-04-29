e61bb5ac56bf32f8143a5b51d02ab88d71b48a3b
sunhao <2080680175@qq.com>
2023-04-29 09:50:59 +0800

    optimizer

--- tools/loss.py
+++ tools/loss.py
@@ -1,12 +1,47 @@
+import torch as t
 import torch.nn as nn
+from torch.autograd import Variable
 
 
 '''
 计算损失
 '''
 class LabelSmoothing(nn.Module):
-    def __init__(self) -> None:
-        super().__init__()
+
+    '''
+    size:   d_model
+    padding_idx:    填充标识符，默认为 0
+    '''
+    def __init__(self, size, padding_idx, smoothing=0.0):
+        super(LabelSmoothing, self).__init__()
+        self.criterion = nn.KLDivLoss(size_average=False)
+        self.padding_idx = padding_idx
+        self.confidence = 1.0 - smoothing
+        self.smoothing = smoothing
+        self.size = size
+        self.true_dist = None
+
+    '''
+    x:      源语言
+    target: 目标语言
+    '''
+    def forward(self, x, target):
+        assert x.size(1) == self.size
+        true_dist = x.data.clone()
+
+        value = self.smoothing / (self.size - 2)
+        # fill_：将 tensor 中的所有值都填充为指定的 value
+        true_dist.fill_(value)
+        # 用来编码 one hot
+        true_dist.scatter_(dim=1, 
+                           index=target.data.unsqueeze(1), 
+                           src=self.confidence)
+        true_dist[:, self.padding_idx] = 0
+        mask = t.nonzero(target.data == self.padding_idx)
+        if mask.dim() > 0:
+            true_dist.index_fill_(0, mask.squeeze(), 0.0)
+        self.true_dist = true_dist
+        return self.criterion(x, Variable(true_dist, requires_grad=False))
 
 
 
---
+++ tools/optim.py
@@ -0,0 +1,55 @@
+import torch as t
+import torch.optim as optim
+
+'''
+优化器
+'''
+class NoamOpt:
+
+    '''
+    Optim wrapper that implements rate.
+        model_size: d_model
+        factor:
+        warmup:
+        optimizer:
+    '''
+    def __init__(self, model_size, factor, warmup, optimizer):
+        self.optimizer = optimizer
+        self._step = 0
+        self.warmup = warmup
+        self.factor = factor
+        self.model_size = model_size
+        self._rate = 0      # 学习率
+
+    '''
+    Update parameters and rate  
+    '''
+    def step(self):
+        self._step += 1
+        rate = self.rate()
+        for p in self.optimizer.param_groups:
+            p['lr'] = rate
+        self._rate = rate
+        self.optimizer.step()
+
+    '''
+    Implement `lrate` above
+    '''
+    def rate(self, step=None):
+        if step is None:
+            step = self._step
+        min_step = min(step**(-0.5), step*self.warmup**(-1.5))
+        out = self.factor * (self.model_size**(-0.5) * min_step)
+        return out
+    
+
+'''
+获取优化器
+'''
+def get_std_opt(model):
+    model_size = model.src_embed[0].d_model
+    factor = 2
+    warmup = 4000
+    optimizer = optim.Adam(model.parameters(), lr=0, betas=(0.9, 0.98), eps=1e-9)
+    opt = NoamOpt(model_size, factor, warmup, optimizer)
+    return opt
